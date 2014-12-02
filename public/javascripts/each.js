da2f5b1365f1147a19b20c098dd6c77c8e41124e
Sam Zagrobelny <strangesast@gmail.com>
2014-12-02 00:18:24 -0500

    added ability to change pixel size

--- public/javascripts/each.js
+++ public/javascripts/each.js
@@ -2,7 +2,11 @@ var _id = document.getElementById('main').getAttribute("name");
 var source;
 var template;
 var details;
-var pixelSize = 6;
+var maxpx = 14;
+var minpx = 2;
+var pixelSize;
+var ctx;
+var canvasElement;
 
 
 $(document).ready(function() {
@@ -29,22 +33,41 @@ function ajaxy(url, obj, callback) {
 }
 
 function buildPage(objectDetails) {
-	console.log(objectDetails);
 	if(objectDetails.length < 1) {
 		$('#main').html("<h1>" + _id + " does not exist.</h1><h4>Sorry</h4>");
 	} else {
 		$('#main').html(template(objectDetails[0]));
 		details = objectDetails[0];
+	  pixelSize = $('#pixelSize').val();
 		beginCanvas();
+		startListeners();
 	}
 }
 
 function beginCanvas() {
-	var canvasElement = document.getElementById('canvas');
+	if(ctx=== undefined) {
+		canvasElement = document.getElementById('canvas');
+		ctx = canvasElement.getContext('2d');
+	}
 	var width = pixelSize*details.hpc;
 	var height = pixelSize*details.vpc;
 	canvasElement.width = width;
 	canvasElement.height = height;
-	var ctx = canvasElement.getContext('2d');
 	ctx.fillRect(0, 0, width, height);
+}
+
+function applyPixelSize(size) {
+	if(size <= maxpx && size >= minpx) {
+	  pixelSize = $('#pixelSize').val();
+		beginCanvas();
+	}
+}
+
+function startListeners() {
+  $('#pixelSize').on('input', function() {
+  	var val = $(this).val();
+		if(val != "") {
+			applyPixelSize(val);
+		};
+  });
 }
